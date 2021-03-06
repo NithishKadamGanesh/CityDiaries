33b6e311fd00fe02f605642ea8033858b81a4d1e
NITHISHKADAM-HUB <80144452+NITHISHKADAM-HUB@users.noreply.github.com>
2021-03-06 11:38:53 +0530

    Add files via upload

---
+++ models/spot.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const SpotSchema = new Schema({
+    title: String,
+    images: [ImageSchema],
+    category: String,
+    description: String,
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+
+
+SpotSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Spot', SpotSchema);
